5406f1170b347ed2d8a2fbcb11312848e3b589c4
sergiocouter <78119200+sergiocouter@users.noreply.github.com>
2022-05-13 17:58:31 -0300

    Update pokemon.py
    Adicionei primeiros comentários do projeto (in início do programa).

---
+++ projeto_em_python/pokemon.py
@@ -0,0 +1,9 @@
+# Desafio Pokemon 
+# Criar um algoritmo que simule uma batalha entre dois pokemons
+# O usuário deverá escolher entre 3 opções de personagem e enfrentar o Pokemom Boss
+# Em cada rodada o usuário pode se curar ou atacar
+# Boss só ataca no turno dele
+# A batalha só acaba quando um dos pokemons morre
+
+
+
