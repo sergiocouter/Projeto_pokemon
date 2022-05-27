cba3833f12050d1c4edae3e5d168208bdfcec6d1
sergiocouter <78119200+sergiocouter@users.noreply.github.com>
2022-05-26 23:03:49 -0300

    confirmar na master

--- projeto_em_python/pokemon.py
+++ projeto_em_python/pokemon.py
@@ -23,12 +23,12 @@ def tempo():
 
 def inicio_do_jogo():
     linha()
-    print("Seja Bem-Vindo a batalha Pokémon")
+    print("Seja Bem-Vindo a Batalha Pokémon ")
     linha()
     tempo()
     jogador = str(input("Qual o seu nome jogador? "))
     linha()
-    print("Ok!! {}, agora vou te passar as opções que tenho para você enfrentar o Mewtho".format(jogador))
+    print("Ok!! {}, agora vou te passar as opções que tenho para você enfrentar o Chefão Mewtho. -\/-".format(jogador))
     tempo()
     limpar()
 
@@ -83,15 +83,182 @@ escolha_do_pokemon = int(input(
 
 if escolha_do_pokemon == 1:
     print("Você escolheu o Pikachu")
-    pikachu(40, 400, 50)
+    ataque_pikachu = 40
+    vida_pikachu = 400
+    cura_pikachu = 50
+    ataque_mewtho = 60
+    vida_mewtho = 500
+    while(vida_pikachu > 0 and vida_mewtho > 0):
+        print("Você tem {} de vida ".format(vida_pikachu))
+        linha()
+        tempo()
+        escolha_turno = input("O que você quer fazer no seu turno? [ A - Atacar ] [ B - Curar ] => ")
+        linha()
+        tempo()
+        if escolha_turno == "A":
+            vida_mewtho = vida_mewtho - ataque_pikachu
+            print("Você atacou o Mewtho e causou {} de dano ".format(ataque_pikachu))
+            print("Mewtho agora tem {} de vida ".format(vida_mewtho))
+            linha()
+            tempo()
+            limpar()
+            print("Agora é o turno do Mewtho -\/- ")
+            linha()
+            tempo()
+            vida_pikachu = vida_pikachu - ataque_mewtho
+            print("O Mewtho te atacou!! ******** \nCausou {} de dano ".format(ataque_mewtho))
+            tempo()
+            limpar()
+
+        elif escolha_turno == "B":
+            vida_pikachu = vida_pikachu + cura_pikachu
+            print("Você usou a cura :) ")
+            linha()
+            print("Você tem agora {} de vida ".format(vida_pikachu))
+            linha()
+            tempo()
+            limpar()
+            print("Agora é o turno do Mewtho -\/- ")
+            linha()
+            vida_pikachu = vida_pikachu - ataque_mewtho
+            print("O Mewtho te atacou!! ****** ")
+            print("Você agora tem {} de vida ".format(vida_pikachu))
+            linha()
+            tempo()
+            limpar()
+        else:
+            print("Você digitou uma opção errada ")
+            print("Agora o Mewtho vai te atacar como punição \nkkkkkkkkkkkkkkk ")
+            linha()
+            tempo()
+            vida_pikachu = vida_pikachu - ataque_mewtho
+            print("O mewtho te atacou! -\/- ")
+            linha()
+            print("Você tem {} de vida ".format(vida_pikachu))
+            tempo()
+            limpar()
+    else:
+        print("Fim de Jogo !!!")        
+
 elif escolha_do_pokemon == 2:
     print("Você escolheu o Bulbasaur")
-    bulbasaur(30, 350, 60)
+    ataque_bulbasaur = 30
+    vida_bulbasaur = 350
+    cura_bulbasaur = 60
+    ataque_mewtho = 60
+    vida_mewtho = 500
+    while(vida_bulbasaur > 0 and vida_mewtho > 0):
+        print("Você tem {} de vida ".format(vida_bulbasaur))
+        linha()
+        tempo()
+        escolha_turno = input("O que você quer fazer no seu turno? [ A - Atacar ] [ B - Curar ] => ")
+        linha()
+        tempo()
+        if escolha_turno == "A":
+            vida_mewtho = vida_mewtho - ataque_bulbasaur
+            print("Você atacou o Mewtho e causou {} de dano ".format(ataque_bulbasaur))
+            print("Mewtho agora tem {} de vida ".format(vida_mewtho))
+            linha()
+            tempo()
+            limpar()
+            print("Agora é o turno do Mewtho -\/- ")
+            linha()
+            tempo()
+            vida_bulbasaur = vida_bulbasaur - ataque_mewtho
+            print("O Mewtho te atacou!! ******** \nCausou {} de dano ".format(ataque_mewtho))
+            tempo()
+            limpar()
+
+        elif escolha_turno == "B":
+            vida_bulbasaur = vida_bulbasaur + cura_bulbasaur
+            print("Você usou a cura :) ")
+            linha()
+            print("Você tem agora {} de vida ".format(vida_bulbasaur))
+            linha()
+            tempo()
+            limpar()
+            print("Agora é o turno do Mewtho -\/- ")
+            linha()
+            vida_bulbasaur = vida_bulbasaur - ataque_mewtho
+            print("O Mewtho te atacou!! ****** ")
+            print("Você agora tem {} de vida ".format(vida_bulbasaur))
+            linha()
+            tempo()
+            limpar()
+        else:
+            print("Você digitou uma opção errada ")
+            print("Agora o Mewtho vai te atacar como punição \nkkkkkkkkkkkkkkk ")
+            linha()
+            tempo()
+            vida_bulbasaur = vida_bulbasaur - ataque_mewtho
+            print("O mewtho te atacou! -\/- ")
+            linha()
+            print("Você tem {} de vida ".format(vida_bulbasaur))
+            tempo()
+            limpar()
+    else:
+        print("Fim de Jogo !!!")
+
 elif escolha_do_pokemon == 3:
     print("Você escolheu o Chameleon")
-    charmeleon(50, 400, 40)
-else:
-    print(
-        "Você digitou um valor incorreto. Por favor digite uma das opções [1] , [2] ou [3]")
+    ataque_charmeleon = 50
+    vida_charmeleon = 400
+    cura_charmeleon = 40
+    ataque_mewtho = 60
+    vida_mewtho = 500
+    while(vida_charmeleon > 0 and vida_mewtho > 0):
+        print("Você tem {} de vida ".format(vida_charmeleon))
+        linha()
+        tempo()
+        escolha_turno = input("O que você quer fazer no seu turno? [ A - Atacar ] [ B - Curar ] => ")
+        linha()
+        tempo()
+        if escolha_turno == "A":
+            vida_mewtho = vida_mewtho - ataque_charmeleon
+            print("Você atacou o Mewtho e causou {} de dano ".format(ataque_charmeleon))
+            print("Mewtho agora tem {} de vida ".format(vida_mewtho))
+            linha()
+            tempo()
+            limpar()
+            print("Agora é o turno do Mewtho -\/- ")
+            linha()
+            tempo()
+            vida_charmeleon = vida_charmeleon - ataque_mewtho
+            print("O Mewtho te atacou!! ******** \nCausou {} de dano ".format(ataque_mewtho))
+            tempo()
+            limpar()
+
+        elif escolha_turno == "B":
+            vida_charmeleon = vida_charmeleon + cura_charmeleon
+            print("Você usou a cura :) ")
+            linha()
+            print("Você tem agora {} de vida ".format(vida_charmeleon))
+            linha()
+            tempo()
+            limpar()
+            print("Agora é o turno do Mewtho -\/- ")
+            linha()
+            vida_charmeleon = vida_charmeleon - ataque_mewtho
+            print("O Mewtho te atacou!! ****** ")
+            print("Você agora tem {} de vida ".format(vida_charmeleon))
+            linha()
+            tempo()
+            limpar()
+        else:
+            print("Você digitou uma opção errada ")
+            print("Agora o Mewtho vai te atacar como punição \nkkkkkkkkkkkkkkk ")
+            linha()
+            tempo()
+            vida_charmeleon = vida_charmeleon - ataque_mewtho
+            print("O mewtho te atacou! -\/- ")
+            linha()
+            print("Você tem {} de vida ".format(vida_charmeleon))
+            tempo()
+            limpar()
+    else:
+        print("Fim de Jogo !!!")
+
+
+print("Obrigado por jogar a Batalha Pokémon !! - _ - ")
 
 
